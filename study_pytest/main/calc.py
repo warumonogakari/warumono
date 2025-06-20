ce9a1199440dae046b3817caf7a7981266f00bf0
warumonogakari <hiroshi_jkato@nifty.com>
2025-06-20 15:51:44 +0900

    [Add] study_pytest

---
+++ study_pytest/main/calc.py
@@ -0,0 +1,17 @@
+class Calc: 
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def dif(self):
+        return self.a - self.b
+
+    def seki(self):
+        return self.a*self.b
+
+    def shou(self):
+        return self.a/self.b
